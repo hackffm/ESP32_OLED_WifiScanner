5f0d90958cca9027fd8ee85c2bf7380bba05c087
tuet <gitdob.9.keks@spamgourmet.com>
2018-02-11 22:16:35 +0100

    readme edited

--- ESP32_OLED_WifiScanner.ino
+++ ESP32_OLED_WifiScanner.ino
@@ -7,36 +7,33 @@ SSD1306 display (0x3c, 5, 4);
 void setup()
 {
     Serial.begin(115200);
-
-    pinMode(LED, OUTPUT);
     
     // Set WiFi to station mode and disconnect from an AP if it was previously connected
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
     delay(100);
-
     Serial.println("Setup done");
 
-    display.init ();
+    display.init();
     display.flipScreenVertically ();
-    display.setFont (ArialMT_Plain_10);
-    delay(1000);
     display.clear();
     display.setTextAlignment (TEXT_ALIGN_LEFT);
-    display.setFont (ArialMT_Plain_10);
+    display.setFont(ArialMT_Plain_10);
 }
 
 void loop()
 {
     Serial.println("scan start");
-
-    display.drawString(0, 0, "Scanning...");
+    display.setColor(BLACK);
+    display.fillRect(0,0,127,10);
+    display.setColor(WHITE);
+    display.drawString(0, 0, "Scanning...     ");
     display.display();
     
     // WiFi.scanNetworks will return the number of networks found
     int n = WiFi.scanNetworks();
     display.clear();
-    display.drawString(0, 0, "Scanning...");
+    display.drawString(0, 0, "Scan results:");
     display.display();    
     Serial.println("scan done");
     if (n == 0) {
@@ -60,7 +57,6 @@ void loop()
         }
     }
     Serial.println("");
-
     display.display();
     // Wait a bit before scanning again
     delay(5000);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ESP32_OLED_WifiScanner
-Wifi scanner for ESP32 modules with OLED display SSD1306
+Wifi scanner for ESP32 modules with OLED display SSD1306. Needs the SSD1306 library from https://github.com/ThingPulse/esp8266-oled-ssd1306 
+
+
